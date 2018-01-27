e39f6922f8fbdd1426c49ffb64406f2238d6cf3d
Oskari Mäki <ma.oskari@gmail.com>
2018-01-28 00:27:31 +0200

    toggleaudio

--- GGJ2018/Assets/Scripts/SoundToggle.cs
+++ GGJ2018/Assets/Scripts/SoundToggle.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class SoundToggle : MonoBehaviour {
 
 
+    public AudioSource audioClip;
+
+    public bool isOn = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +16,18 @@ public class SoundToggle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if(isOn == true)
+        {
+            PlayAudio();
+        }
 		
 	}
+
+    void PlayAudio()
+    {
+        audioClip.Play();
+        isOn = false;
+    }
+
 }
