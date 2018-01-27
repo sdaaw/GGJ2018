c37fdff80b8d5486d4567812231c716231cf3d2c
Ati Marin <ati.marin1@gmail.com>
2018-01-27 14:06:53 +0200

    added shit menu

---
+++ GGJ2018/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
