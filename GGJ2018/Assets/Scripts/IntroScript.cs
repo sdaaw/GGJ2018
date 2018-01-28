38b77d5e2c873ec7cfe966ebf0f90794dd7f1282
Ati Marin <ati.marin1@gmail.com>
2018-01-28 14:53:53 +0200

    done

---
+++ GGJ2018/Assets/Scripts/IntroScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroScript : MonoBehaviour
+{
+    private Animator m_a;
+
+    private void Awake()
+    {
+        m_a = GetComponent<Animator>();
+        UnityEngine.XR.XRSettings.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (m_a.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            Application.LoadLevel("testings");
+    }
+}
